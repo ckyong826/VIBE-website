fc38677870a878054e4166086a96576b2f2c6cdb
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2023-10-25 11:23:49 +0800

    navbar done

--- vibe/app/components/Navbar.tsx
+++ vibe/app/components/Navbar.tsx
@@ -10,36 +10,40 @@ import { useState,useEffect } from 'react'
 export default function Navbar(){
   const Links = [
     {name:"HOME",link:"/"},
-    {name:"SHOP",link:"/"},
-    {name:"STORY",link:"/"},
-    {name:"FAQs",link:"/"},
-    {name:"ABOUT",link:"/"},
+    {name:"SHOP",link:"/shop"},
+    {name:"STORY",link:"/story"},
+    {name:"FAQs",link:"/faqs"},
+    {name:"ABOUT",link:"/about"},
   ]
 
   const [isMenuOpen, setIsMenuOpen] = useState<Boolean>(false)
 
   const router = useRouter()
   return(
-    <div className='fixed bg-[#1D1D1F]'>
+    <div className='fixed bg-t-black'>
       <nav className="w-screen h-[100px] flex text-t-white items-center justify-between top-0">
         <div className='pl-[50px] w-[150px] h-auto block '>
         <Image src={Logo} alt="VIBE-streetwear" className='object-scale-down items-center cursor-pointer' onClick={() => router.push('/')}/>
         </div>
         <ul className={`w-[600px] text-[30px] flex font-roboto-con font-medium items-center justify-center 
-                       transition-all duration-200 ease-in-out max-lg:w-[50%] max-sm:w-screen max-lg:h-screen 
-                       max-lg:block max-lg:absolute max-lg:justify-end max-lg:top-[100px] max-lg:z-[999]
-                       max-lg:
+                       transition-all duration-200 ease-in-out max-lg:w-screen max-lg:h-screen  max-lg:pt-[100px]
+                       max-lg:block max-lg:absolute max-lg:justify-end max-lg:top-0 max-lg:z-[999]
+                       max-lg:bg-t-black
                        ${isMenuOpen?'max-lg:right-0':'max-lg:right-[-100%] '}
                        `}>
           {Links.map((link,index) => {
             return(
               <Link href={link.link} key={index} className='mx-5 hover:text-t-yellow text-center transition duration-700 ease-in-out 
-                                                            inline-block max-lg:w-[100%]  '>
-                                          {link.name}</Link>
+                                                            inline-block max-lg:w-[100%] max-lg:mx-0 truncate 
+                                                            '>
+                                          {link.name}
+                                          </Link>
+                                          
+                                          
             )
           })}
           <Link href="/account"className='mx-5 hover:text-t-yellow transition text-center  duration-700 ease-in-out 
-                                        inline-block max-lg:w-[100%] lg:hidden'>
+                                        inline-block max-lg:w-[100%] lg:hidden max-lg:mx-0 '>
                                           ACCOUNT</Link>
         </ul>
 
@@ -50,7 +54,7 @@ export default function Navbar(){
           <button onClick={() => router.push('/cart')} className=''>
             <FontAwesomeIcon icon={faShoppingCart} className='mx-2 w-[20px] h-[20px] max-lg:w-[27px] max-lg:h-[27px] hover:text-t-yellow transition duration-700 ease-in-out'/>
           </button>
-          <button onClick={()=>setIsMenuOpen(!isMenuOpen)} className='hidden max-lg:block'>
+          <button onClick={()=>setIsMenuOpen(!isMenuOpen)} className='hidden max-lg:block z-[999]' >
             <FontAwesomeIcon icon={isMenuOpen?faClose:faBars} className='mx-2 w-[30px] h-[30px] hover:text-t-yellow transition duration-700 ease-in-out' />
           </button>
         </ul>
