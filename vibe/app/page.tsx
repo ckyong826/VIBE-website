ba638afe9d49db5ccb9261457b0959b4ce734c11
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2023-10-25 11:51:14 +0800

    update

--- vibe/app/page.tsx
+++ vibe/app/page.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>HomePage</div>
+    <div>
+      <div>
+        <h1>NEW</h1>
+        
+      </div>
+      <div>
+        <h1>ARRIVAL</h1>
+      </div>
+    </div>
   )
 }
